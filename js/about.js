64aa846857b62d8f1b5a900ea4de6809e6a42a23
Vortex <vortexsnail12@gmail.com>
2025-10-23 11:54:36 -0700

    initial commit 2

--- js/about.js
+++ js/about.js
@@ -12,6 +12,19 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	//document.getElementById('firstDate').innerText = tweet_array.length;	
+	//document.getElementById('lastDate').innerText = tweet_array.length;	
+	//document.getElementsByClassName('completedEvents')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('completedEventsPct')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('completedEvents')[1].innerText = tweet_array.length;
+	//document.getElementsByClassName('liveEvents')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('achievements')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('miscellaneous')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('liveEventsPct')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('achievementsPct')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('miscellaneousPct')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('written')[0].innerText = tweet_array.length;
+	//document.getElementsByClassName('writtenPct')[0].innerText = tweet_array.length;
 }
 
 //Wait for the DOM to load
